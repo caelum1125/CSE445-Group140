187ee54bdaf789e47511a02f513be86a2ed4d8b8
CSondrup <156858044+CSondrup@users.noreply.github.com>
2025-04-27 02:41:39 -0700

    captcha

---
+++ CaptchaTryIt.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CaptchaTryIt.aspx.cs" Inherits="Captcha.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="imgCaptcha" runat="server" Width="150px" Height="50px"/>
+            <asp:TextBox ID="txtCaptcha" runat="server" placeholder="Enter CAPTCHA text" Width="150px"></asp:TextBox>
+            <asp:Button ID="btnRefresh" runat="server" Text="Refresh" OnClick="btnRefresh_Click"/>
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+            <asp:Button ID="validateCaptcha" runat="server" Text="Enter" OnClick="btnValidate_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CaptchaTryIt.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+
+namespace Captcha
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                GenerateCaptcha();
+        }
+
+        protected void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GenerateCaptcha();
+        }
+
+        private void GenerateCaptcha()
+        {
+            // Generate random 5-character CAPTCHA
+            Random rand = new Random();
+            string captchaText = new string(
+                Enumerable.Repeat("ABCDEFGHJKLMNPQRSTUVWXYZ23456789", 5)
+                .Select(s => s[rand.Next(s.Length)]).ToArray());
+
+            // Store in Session for validation
+            Session["Captcha"] = captchaText;
+
+            // Generate image
+            Bitmap bmp = new Bitmap(150, 50);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.DrawString(captchaText, new Font("Arial", 20, FontStyle.Bold), Brushes.Black, 10, 10);
+
+            // Add noise
+            for (int i = 0; i < 500; i++)
+                bmp.SetPixel(rand.Next(bmp.Width), rand.Next(bmp.Height), Color.Gray);
+
+            // Convert to base64 for display
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            imgCaptcha.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+        }
+
+        // Public method to validate user input
+        public bool ValidateCaptcha()
+        {
+            if (Session["Captcha"] == null || txtCaptcha.Text != Session["Captcha"].ToString())
+            {
+                lblMessage.Text = "Invalid CAPTCHA!";
+                GenerateCaptcha(); // Refresh CAPTCHA on failure
+                return false;
+            }
+            lblMessage.Text = "CAPTCHA verified!";
+            return true;
+        }
+        protected void btnValidate_Click(object sender, EventArgs e)
+        {
+            ValidateCaptcha();
+        }
+    }
+}
