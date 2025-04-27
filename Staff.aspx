4eea2e92a210683e3126ed66362c3c0259083f71
Ty Stinson <StinsonViolet1@gmail.com>
2025-04-27 16:52:07 -0700

    Add files via upload

---
+++ Staff.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Staff.aspx.cs" Inherits="StaffWebApp.Staff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Staff Management</h2>
+
+<asp:Label ID="lblUsername" runat="server" Text="Username:" />
+<asp:TextBox ID="txtUsername" runat="server" /><br /><br />
+
+<asp:Label ID="lblPassword" runat="server" Text="Password:" />
+<asp:TextBox ID="txtPassword" runat="server" TextMode="Password" /><br /><br />
+
+<asp:Button ID="btnAddStaff" runat="server" Text="Add Staff" OnClick="btnAddStaff_Click" />
+<asp:Button ID="btnViewStaff" runat="server" Text="View Staff" OnClick="btnViewStaff_Click" /><br /><br />
+
+<asp:Label ID="lblResult" runat="server" ForeColor="Green" /><br />
+
+<asp:GridView ID="GridViewStaff" runat="server" AutoGenerateColumns="true"></asp:GridView>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Staff.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace StaffWebApp
+{
+    public partial class Staff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnAddStaff_Click(object sender, EventArgs e)
+        {
+            //retrieve username and password from text
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                lblResult.Text = "Username and Password cannot be empty.";
+                return;
+            }
+
+            string encryptedPassword = password; //replace later
+
+            //defines path to xml file
+            string path = Server.MapPath("~/Staff.xml");
+
+            XDocument doc;
+
+            //check if staff.xml exists
+            if (File.Exists(path))
+            {
+                doc = XDocument.Load(path);
+            }
+            else
+            {
+                doc = new XDocument(new XElement("StaffMembers"));
+            }
+
+            //adds a new <Staff> entry
+            doc.Root.Add(
+                new XElement("Staff",
+                    new XElement("Username", username),
+                    new XElement("Password", encryptedPassword)
+                )
+            );
+
+            //saves changes
+            doc.Save(path);
+
+            lblResult.Text = "Staff added successfully!";
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        protected void btnViewStaff_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("~/Staff.xml");
+            if (File.Exists(path))
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(path);
+                GridViewStaff.DataSource = ds.Tables[0];
+                GridViewStaff.DataBind();
+            }
+        }
+    }
+}
