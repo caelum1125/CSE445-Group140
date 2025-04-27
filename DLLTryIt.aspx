fa653909f0f2c99c91e69a6ab39040ab30a5aa5c
CSondrup <156858044+CSondrup@users.noreply.github.com>
2025-04-27 02:42:07 -0700

    dll

---
+++ DLLTryIt.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DLLTryIt.aspx.cs" Inherits="DLL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtInput" runat="server"></asp:TextBox>
+            <asp:TextBox ID="txtKey" runat="server" placeholder="Encryption Key"></asp:TextBox>
+            <asp:Label ID="lblResult" runat="server" Text="Label"></asp:Label>
+            <asp:Button ID="btnHash" runat="server" Text="Hash" OnClick="btnHash_Click" />
+            <asp:Button ID="btnEncrypt" runat="server" Text="Encrypt" OnClick="btnEncrypt_Click" />
+            <asp:Button ID="btnDecrypt" runat="server" Text="Decrypt" OnClick="btnDecrypt_Click" />
+            <asp:Button ID="btnGenerate" runat="server" Text="Generate Password" OnClick="btnGenerate_Click" />
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DLLTryIt.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SecurityLibrary; // Reference your DLL namespace
+
+namespace DLL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnHash_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = SecurityHelper.HashPassword(txtInput.Text);
+        }
+
+        protected void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = SecurityHelper.Encrypt(txtInput.Text, txtKey.Text);
+        }
+
+        protected void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = SecurityHelper.Decrypt(txtInput.Text, txtKey.Text);
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Generate a random key
+            GeneratePassword.Service1Client genPass = new GeneratePassword.Service1Client();
+            string password = genPass.GeneratePassword();
+            txtPassword.Text = password;
+        }
+    }
+}
