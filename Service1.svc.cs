993462b30d8f446671fa4717d5a5ee2f818f0590
CSondrup <156858044+CSondrup@users.noreply.github.com>
2025-04-28 14:13:18 -0700

    pass gen

---
+++ Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PasswordGenerator
+{
+   
+    public class Service1 : IService1
+    {
+        public string GeneratePassword()
+        {
+            int length = 12;
+            bool includeNumbers = true;
+            bool includeSpecialChars = true;
+
+            const string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numbers = "0123456789";
+            const string specials = "!@#$%^&*()-_=+";
+
+            string validChars = lowercase + uppercase;
+
+            if (includeNumbers) validChars += numbers;
+            if (includeSpecialChars) validChars += specials;
+
+            byte[] randomBytes = new byte[length];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes);
+            }
+
+            char[] password = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = validChars[randomBytes[i] % validChars.Length];
+            }
+
+            return new string(password);
+        }
+    }
+}
